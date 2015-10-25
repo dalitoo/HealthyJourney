908dcd1f593bcd0d8fa99faffa20929d13f961bc
rebel.saidi <rebel089@outlook.com>
2015-10-25 14:59:26 +0100

    adding the infrastracture repo /n the factory class, the disposable class and the UnitOfWoefefzfez7f

---
+++ HealthyJourney.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using HealthyJourney.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthyJourney.Data.Infrastructure
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private HealthyJourneyContext dataContext;
+		IDatabaseFactory dbFactory;
+		public UnitOfWork(IDatabaseFactory dbFactory)
+		{
+			this.dbFactory = dbFactory;
+		}
+		private IProviderRepository providerRepository;
+		public IProviderRepository ProviderRepository
+		{
+			get { return providerRepository = new ProviderRepository(dbFactory); }
+		}
+		private ICategoryRepository categoryRepository;
+		public ICategoryRepository CategoryRepository
+		{
+			get { return categoryRepository = new CategoryRepository(dbFactory); }
+		}
+		private IProductRepository productRepository;
+		public IProductRepository ProductRepository
+		{
+			get { return productRepository = new ProductRepository(dbFactory); }
+		}
+		protected MyFinanceContext DataContext
+		{
+			get
+			{
+				return dataContext = dbFactory.DataContext;
+			}
+		}
+		public void Commit()
+		{
+			DataContext.SaveChanges();
+		}
+		public void Dispose()
+		{
+			dbFactory.Dispose();
+		}
+	}
+
+}
